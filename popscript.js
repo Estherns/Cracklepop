595312467858ad9bc632181a11611a4c71f84933
Esther <24371125+Estherns@users.noreply.github.com>
2019-03-25 16:55:55 +0300

    Add files via upload

---
+++ popscript.js
@@ -0,0 +1,18 @@
+
+/*Rc sample code to write a program that prints out the numbers 1 to 100 (inclusive),if the number is divisible by 3, print Crackle instead of the number,
+if it's divisible by 5, print Pop, if it's divisible by both 3 and 5, print CracklePop. */
+function cracklePop() {
+	for (var i = 1; i <= 100; i++) {
+		if (i % 15 === 0) {
+			document.write("CracklePop"+"<br>");
+		} else if (i % 5 === 0) {
+			document.write("Pop"+"<br>");
+		} else if (i % 3 === 0){
+			document.write("Crackle"+"<br>");
+		} else {
+			document.write(i+"<br>");
+		}
+	}
+}
+cracklePop();
+
