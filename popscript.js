b9eacbf13b9f2039baf6aa4f98915a7d23e435b6
Esther <24371125+Estherns@users.noreply.github.com>
2019-04-04 14:29:51 +0300

    Update popscript.js

--- popscript.js
+++ popscript.js
@@ -3,8 +3,10 @@
 if it's divisible by 5, print Pop, if it's divisible by both 3 and 5, print CracklePop. */
 function cracklePop() {
 	for (var i = 1; i <= 100; i++) {
-		if (i % 15 === 0) {
+		if (i % 3 === 0 && i % 5 === 0) {
 			document.write("CracklePop"+"<br>");
+	              /* You can also use the option below
+		      if (i % 15 === 0) */
 		} else if (i % 5 === 0) {
 			document.write("Pop"+"<br>");
 		} else if (i % 3 === 0){
